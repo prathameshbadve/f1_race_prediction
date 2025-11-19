0b7a36e8ac0f33fe5fc97763b767577e2afa2620
Prathamesh Badve <padve.96@gmail.com>
2025-11-19 12:37:10 +0530

    added a db for hosting processed data in dev env

--- scripts/database/init-db.sql
+++ scripts/database/init-db.sql
@@ -4,6 +4,10 @@ CREATE DATABASE mlflow;
 -- Create database for Dagster
 CREATE DATABASE dagster;
 
+-- Create database for data warehouse
+CREATE DATABASE data_warehouse;
+
 -- Grant privileges (optional, since we're using the postgres superuser)
 GRANT ALL PRIVILEGES ON DATABASE mlflow TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE dagster TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE data_warehouse TO postgres;
