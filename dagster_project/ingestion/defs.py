b26de9651ed5d7c76867aac05474c5e8ace846f4
Prathamesh Badve <padve.96@gmail.com>
2025-11-25 19:39:04 +0530

    add assets and sensors to ingestion definitions

--- dagster_project/ingestion/defs.py
+++ dagster_project/ingestion/defs.py
@@ -4,12 +4,24 @@ Dagster definitions for ingestion module.
 
 from dagster import Definitions
 
+from dagster_project.ingestion.assets import season_schedule, session_results
+from dagster_project.ingestion.resources import FastF1Resource
+from dagster_project.ingestion.sensors import (
+    update_partitions_on_schedule_materialization,
+)
 from dagster_project.shared.resources import BucketResource, RedisResource
 
 defs = Definitions(
-    assets=[],
+    assets=[
+        season_schedule,
+        session_results,
+    ],
     resources={
         "bucket_resource": BucketResource.from_env(),
         "redis_resource": RedisResource.from_env(),
+        "fastf1_resource": FastF1Resource.from_env(),
     },
+    sensors=[
+        update_partitions_on_schedule_materialization,
+    ],
 )
