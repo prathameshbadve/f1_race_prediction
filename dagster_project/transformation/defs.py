31455bdf78918e2dd287c90cf4e9ce0b4ba0088d
Prathamesh Badve <padve.96@gmail.com>
2025-12-04 19:01:22 +0530

    data transformation assets

---
+++ dagster_project/transformation/defs.py
@@ -0,0 +1,15 @@
+"""
+Dagster definitions for transformation module.
+"""
+
+from dagster import Definitions
+
+from dagster_project.shared.resources import BucketResource, RedisResource
+
+defs = Definitions(
+    assets=[],
+    resources={
+        "bucket_resource": BucketResource.from_env(),
+        "redis_resource": RedisResource.from_env(),
+    },
+)
